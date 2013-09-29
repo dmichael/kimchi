39d9752217814237b156375b1a61fb41ac3f7c7d
David Michael <david.michael@gmail.com>
2013-09-28 20:54:01 -0400

    new mapping api

--- lib/kimchi/mapping.rb
+++ lib/kimchi/mapping.rb
@@ -1,15 +1,42 @@
 module Kimchi
   module Mapping
+    extend ActiveSupport::Concern
+    
+    module Registry
+      def self.mappings
+        @mappings ||= []  
+      end
 
-  	def self.included klass
-      @mappings ||= []
-      @mappings << klass
+      def self.dump
+        mappings.reduce({}) do |hash, klass|
+          hash[klass.get_type] = klass.get_mapping
+          hash
+        end
+      end
     end
 
-    def self.mappings
-      @mappings.reduce({}) do |hash, klass|
-        hash[klass.type] = klass.mapping
-        hash
+    def self.included klass
+      Registry.mappings << klass
+      klass.class_eval do
+        extend ClassMethods
+      end
+    end
+
+    module ClassMethods
+      def get_type
+        @type
+      end
+
+      def get_mapping
+        @mapping
+      end
+
+      def type(name)
+        @type = name
+      end
+
+      def mapping(&block)
+        @mapping = yield
       end
     end
 
